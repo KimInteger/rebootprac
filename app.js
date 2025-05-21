4849b03c16693ca04f98e29e4d848bf83c0a57ad
김정수 <kjs20151240@gmail.com>
2025-05-21 18:59:07 +0900

    add | html파일과 js파일 호스팅 완료

--- app.js
+++ app.js
@@ -3,8 +3,31 @@ const fs = require('fs');
 const path = require('path');
 const PORT = 3000;
 
-const server = http.createServer((res,req)=>{
-  console.log(res.url);
+const server = http.createServer((req,res)=>{
+  console.log(req.url);
+  if(req.url === '/'){
+    const filePath = path.join(__dirname, 'index.html');
+    fs.readFile(filePath,(err,data)=>{
+      if(err){
+        res.writeHead(500, {"Content-Type":"text/plain"});
+        res.end("잘못된 접근")
+      } else {
+        res.writeHead(200,{"Content-Type" : "text/html"})
+        res.end(data)
+      }
+    })
+  } else if (req.url==='/index.js'){
+    const filePath = path.join(__dirname,'index.js');
+    fs.readFile(filePath,(err,data)=>{
+      if(err){
+        res.writeHead(500, {"Content-Type":"text/plain"});
+        res.end("잘못된 접근")
+      } else {
+        res.writeHead(200,{"Content-Type" : "application/javascript"})
+        res.end(data)
+      }
+    })
+  }
 })
 
 
