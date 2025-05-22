7c9a2e9a78815e37ba8ac6eb54db70f2150f0ed7
김정수 <kjs20151240@gmail.com>
2025-05-22 18:26:27 +0900

    add | module.js생성 mime-types를 활용하여 정적파일 서빙하는 함수 생성

--- module.js
+++ module.js
@@ -1,1 +1,21 @@
-console.log('module화 작업 밑준비')
+const mime = require('mime-types')
+const fs = require('fs')
+const path = require('path')
+
+function serveStaticFile(res, filePath) {
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      res.writeHead(404, { "Content-Type": "text/plain" });
+      res.end("파일을 찾을 수 없습니다.");
+    } else {
+      console.log(filePath)
+      const contentType = mime.lookup(filePath) || 'application/octet-stream';
+      res.writeHead(200, { "Content-Type": contentType });
+      res.end(data);
+    }
+  });
+}
+
+module.exports = {
+  serveStaticFile,
+};
