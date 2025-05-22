e567893a712523b91f053dc6629a25e142c286e5
김정수 <kjs20151240@gmail.com>
2025-05-22 18:26:55 +0900

    modi | app.js 코드 단축화 및 함수 호출

--- app.js
+++ app.js
@@ -1,33 +1,17 @@
 const http = require('http');
 const fs = require('fs');
 const path = require('path');
+const {serveStaticFile} = require('./module')
 const PORT = 3000;
 
 const server = http.createServer((req,res)=>{
-  console.log(req.url);
-  if(req.url === '/'){
-    const filePath = path.join(__dirname, 'index.html');
-    fs.readFile(filePath,(err,data)=>{
-      if(err){
-        res.writeHead(500, {"Content-Type":"text/plain"});
-        res.end("잘못된 접근")
-      } else {
-        res.writeHead(200,{"Content-Type" : "text/html"})
-        res.end(data)
-      }
-    })
-  } else if (req.url==='/index.js'){
-    const filePath = path.join(__dirname,'index.js');
-    fs.readFile(filePath,(err,data)=>{
-      if(err){
-        res.writeHead(500, {"Content-Type":"text/plain"});
-        res.end("잘못된 접근")
-      } else {
-        res.writeHead(200,{"Content-Type" : "application/javascript"})
-        res.end(data)
-      }
-    })
-  }
+  console.log(` 요청 확인 : ${req.url}`);
+
+  let filePath = req.url === '/' ? './index.html' : `.${req.url}`;
+  filePath = path.join(__dirname, filePath);
+  console.log(filePath)
+
+  serveStaticFile(res, filePath);
 })
 
 
@@ -35,6 +19,6 @@ server.listen(PORT,(err)=>{
   if(err){
     console.log('err');
   } else {
-    console.log('server open')
+    console.log(`server open : http://localhost:${PORT}`)
   }
 })
